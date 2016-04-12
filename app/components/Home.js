f05a4377dc25e4aa716667fa46f41197478a7164
Seamus O'Keefe <okeefe77@gmail.com>
2016-04-12 18:08:37 +0000

    49% complete with course

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,18 @@
-var React = require('react');
+var React = require('react'),
+    ReactRouter = require('react-router'),
+    Link = ReactRouter.Link,
+    transparentBg = require('../styles').transparentBg;
 
 var Home = React.createClass({
     render: function() {
         return (
-            <div>Hello from the Home component.</div>
+            <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+                <h1>GitHub Battle</h1>
+                <p className="lead">Some motto.</p>
+                <Link to="/PlayerOne">
+                    <button type="button" className="btn btn-large btn-success">Get Started</button>
+                </Link>
+            </div>
         )
     }
 });
